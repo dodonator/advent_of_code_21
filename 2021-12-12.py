30b711ac74fd71fc61cf7cf99115bbd7f7417e39
dodonator <dodo@chaosdorf.de>
2022-12-02 23:12:04 +0100

    day 12: generate a graph

--- 2021-12-12.py
+++ 2021-12-12.py
@@ -1,9 +1,26 @@
 # https://adventofcode.com/2021/day/12
 
 from pathlib import Path
+import networkx as nx
+import matplotlib.pyplot as plt
 
 filename = f"input_" + Path(__file__).stem + ".txt"
 filepath = Path(filename)
 
+nodes = set()
+edges = set()
+
 with filepath.open("r") as stream:
-    pass
+    for line in stream:
+        node1, node2 = line.strip("\n").split("-")
+        edge = frozenset((node1, node2))
+        nodes.update(edge)
+        edges.add(edge)
+
+graph = nx.Graph()
+
+graph.add_nodes_from(nodes)
+graph.add_edges_from(edges)
+
+nx.draw(graph, with_labels=True)
+plt.show()
